9df9efbee4ba27206ec1d7e2250318241d0eb217
Suraj <surajkukati95@gmail.com>
2019-09-25 16:41:14 +0530

    style updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
 //import Dictionary from './dictionary.json';
 
@@ -7,6 +7,9 @@ class App extends React.Component {
   state = {
     searchTerm: '',
     result: '',
+    resultTitle: '',
+    errorMessage: 'Nope, nothing found. Try another word.',
+    showError: false,
   }
   componentDidMount() {
     //console.log(Dictionary['DIPLOBLASTIC']);
@@ -20,42 +23,61 @@ class App extends React.Component {
   handleSearch = (event) => {
   event.preventDefault();
   let searchTerm = this.state.searchTerm.toLowerCase();
-  //const Dictionary =  import(`./dictionary/${searchTerm[0].toLowerCase()}.json`);
+  this.setState({ resultTitle: searchTerm, result: '' });
 
   import(`./dictionary/${searchTerm[0]}.json`)
       .then(( Dictionary ) => {
-        // Use dictionary
-        //console.log(Dictionary.default[searchTerm]);
-        this.setState({ result: Dictionary.default[searchTerm] });
+        let result = Dictionary.default[searchTerm];
+        if(typeof result === 'undefined'){
+          this.setState({ showError: true });
+        }
+        else {
+          this.setState({ resultTitle: searchTerm, result, showError: false });
+        } 
       })
       .catch(err => {
         // Handle failure
         console.log(`error`, err);
+        this.setState({ showError: true })
       });
 }
 
   render() {
     return (
       <div className="App">
-        Darktionary
-
-        <form onSubmit={this.handleSearch}>
-          <input
-            type="text"
-            placeholder="Search here"
-            onChange={event=>{this.handleChange(event)}}
-            value={this.state.searchTerm}
-          ></input>
-          <button type="submit" onClick={this.handlesearch}>
-            Search
-          </button>
-        </form>
-        <>
+          <form onSubmit={this.handleSearch}>
+            <div className="searchForm" >
+              <input
+                className="searchInput"
+                type="text"
+                placeholder="Search here"
+                onChange={event=>{this.handleChange(event)}}
+                value={this.state.searchTerm}
+              ></input>
+              <button className="searchButton" type="submit" onClick={this.handlesearch}>
+                <img className="searchIcon" alt="search-icon" src="https://icon-library.net/images/white-search-icon-transparent-background/white-search-icon-transparent-background-1.jpg" />
+              </button>
+            </div>
+          </form>
           {
-            (this.state.result !== '') &&
-            this.state.result
+           (this.state.result) &&
+            <div className="searchResults">
+                <div className="resultTitle">
+                  {this.state.resultTitle}
+                </div>
+                <div className="titleUnderline">
+                </div>
+                <div className="searchContent">
+                  {this.state.result}
+                </div>
+            </div>
           }
-        </>
+          {
+            (this.state.showError) &&
+            <div className="errorMessage">
+              {this.state.errorMessage}
+            </div>
+          }
       </div>
     );
   }
