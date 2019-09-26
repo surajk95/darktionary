71de544fabd236ab2d515dc1bc88a98621336e4a
Suraj <surajkukati95@gmail.com>
2019-09-26 16:40:09 +0530

    replaced dictionary

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ class App extends React.Component {
   state = {
     landing: true,
     searchTerm: '',
-    result: '',
+    result: {},
     resultTitle: '',
     errorMessage: 'Nope, nothing found. Try another word.',
     showError: false,
   }
   componentDidMount() {
-    //console.log(Dictionary['DIPLOBLASTIC']);
+    
   }
 
   handleChange = (event) => {
@@ -23,17 +23,18 @@ class App extends React.Component {
 
   handleSearch = (event) => {
   event.preventDefault();
-  let searchTerm = this.state.searchTerm.toLowerCase();
+  let searchTerm = this.state.searchTerm.toUpperCase();
   this.setState({ landing: false, resultTitle: searchTerm, result: '' });
 
-  import(`./dictionary/${searchTerm[0]}.json`)
+  import(`./dictionary/D${searchTerm[0]}.json`)
       .then(( Dictionary ) => {
         let result = Dictionary.default[searchTerm];
         if(typeof result === 'undefined'){
           this.setState({ showError: true });
         }
         else {
-          this.setState({ resultTitle: searchTerm, result, showError: false });
+          console.log(result);
+          this.setState({ resultTitle: searchTerm, result, showError: false, searchTerm: '' });
         } 
       })
       .catch(err => {
@@ -54,6 +55,7 @@ class App extends React.Component {
                 placeholder="Search here"
                 onChange={event=>{this.handleChange(event)}}
                 value={this.state.searchTerm}
+                autoFocus
               ></input>
               <button className="searchButton" type="submit" onClick={this.handlesearch}>
                 <img className="searchIcon" alt="search-icon" src="https://icon-library.net/images/white-search-icon-transparent-background/white-search-icon-transparent-background-1.jpg" />
@@ -66,10 +68,60 @@ class App extends React.Component {
                 <div className="resultTitle">
                   {this.state.resultTitle}
                 </div>
-                <div className="titleUnderline">
-                </div>
                 <div className="searchContent">
-                  {this.state.result}
+                {
+                  (this.state.result.MEANINGS && Object.values(this.state.result.MEANINGS) && Object.values(this.state.result.MEANINGS).length>0) &&
+                  <>
+                      <div className="titleUnderline">
+                      </div>
+                      {
+                        Object.values(this.state.result.MEANINGS).map((item, index) => {
+                          console.log(item);
+                          return(
+                            <div className="" key={index}>
+                              {
+                                item.length>0 &&
+                                item.map((deeperItem, deeperIndex) => {
+                                  return (
+                                    typeof deeperItem === 'string' &&
+                                    <div key={deeperIndex}>
+                                      {deeperItem}
+                                    </div>
+                                  )
+                                })
+                              }
+                            </div>
+                          )
+                        })
+                      }
+                  </>
+                }
+                {
+                  (this.state.result.SYNONYMS && this.state.result.SYNONYMS.length>0) &&
+                  <>
+                    <div> Synonyms: </div>
+                      {
+                        this.state.result.SYNONYMS.map((item, index) => {
+                        return(
+                          <span className="inlineItem" key={index}>{item}</span>
+                        )
+                      })
+                      }
+                  </>
+                }
+                {
+                  (this.state.result.ANTONYMS && this.state.result.ANTONYMS.length>0) &&
+                  <>
+                    <div> Antonyms: </div>
+                      {
+                        this.state.result.ANTONYMS.map((item, index) => {
+                        return(
+                          <span className="inlineItem" key={index}>{item}</span>
+                        )
+                      })
+                      }
+                  </>
+                }
                 </div>
             </div>
           }
